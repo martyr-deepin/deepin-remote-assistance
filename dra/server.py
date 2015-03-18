5572dd03b86f313f543d519b30a82e83d13708d3
Xu Shaohua <xushaohua@linuxdeepin.com>
2015-03-18 11:19:13 +0800

    Fixed: typo, chomium -> chromium

--- dra/server.py
+++ dra/server.py
@@ -1,7 +1,8 @@
 
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtCore import QObject
 from dra.chromium import Chromium
-from dra.wssd import WSSD
+from dra.wssd import WSSDController
 
 '''
 Controller for service side.
@@ -17,6 +18,8 @@ Stop:
 
 
 class Server(QObject):
+
+    browserCmd = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
@@ -36,8 +39,10 @@ class Server(QObject):
     def start_wssd(self):
         '''Start websocket service'''
         self.stop_wssd()
-        self.wssd = WSSD()
+        self.wssd = WSSDController(self)
         self.wssd.start()
+        self.wssd.worker.browserCmd.connect(
+                lambda msg: self.browserCmd.emit(msg))
 
     def stop_wssd(self):
         '''Stop websocket service'''
@@ -47,10 +52,10 @@ class Server(QObject):
     def start_chromium(self):
         '''Launch chromium in background'''
         self.stop_chromium()
-        self.chromium = Chromium()
+        self.chromium = Chromium(self)
         self.chromium.start()
 
     def stop_chromium(self):
         '''Kill chromium'''
         if self.chromium:
-            self.chomium.stop()
+            self.chromium.stop()
