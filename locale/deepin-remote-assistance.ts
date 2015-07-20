f0f6293d0670e14fa8af9c7b90fe86016bcd2cef
Xu Shaohua <xushaohua@linuxdeepin.com>
2015-07-20 16:20:20 +0800

    Add `locale/` directory
    Change-Id: I14c20e48488b0ade614a33fd8ec82c63904d235d

---
+++ locale/deepin-remote-assistance.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>ConfirmWindow</name>
+    <message>
+        <location filename="../dra_server/views/ConfirmWindow.qml" line="+81"/>
+        <source>Are you sure to close remote desktop connection?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+13"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisconnectWindow</name>
+    <message>
+        <location filename="../dra_server/views/DisconnectWindow.qml" line="+141"/>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FullscreenTitleBar</name>
+    <message>
+        <location filename="../dra_client/views/FullscreenTitleBar.qml" line="+32"/>
+        <source>Deepin Remote Assistance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NormalTitleBar</name>
+    <message>
+        <location filename="../dra_client/views/NormalTitleBar.qml" line="+22"/>
+        <source>Deepin Remote Assistance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+8"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
